99c5483ae530238d7d7e558f0910ec204bcafd39
FarhanAli325706 <farhanlala706@gmail.com>
2019-10-14 18:02:26 +0500

    assignment_3

---
+++ base/src/main/java/com/ahmad/cv/base/MyRecycleView.java
@@ -0,0 +1,14 @@
+package com.ahmad.cv.base;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MyRecycleView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_recycle_view);
+    }
+}
