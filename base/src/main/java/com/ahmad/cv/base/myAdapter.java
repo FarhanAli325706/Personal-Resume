c2df483b9952db90199f21adebd12022459d8447
FarhanAli325706 <farhanlala706@gmail.com>
2019-10-14 18:58:02 +0500

    adapterComplete

--- base/src/main/java/com/ahmad/cv/base/myAdapter.java
+++ base/src/main/java/com/ahmad/cv/base/myAdapter.java
@@ -1,5 +1,7 @@
 package com.ahmad.cv.base;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
@@ -7,22 +9,32 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.LinkedList;
+
 public class myAdapter extends RecyclerView.Adapter<myAdapter.myViewHolder> {
 
+    private final LinkedList<String> skillList;
+    private LayoutInflater myInflater;
+    public myAdapter(Context context, LinkedList<String> wordList) {
+        myInflater = LayoutInflater.from(context);
+        this.skillList = wordList;
+    }
     @NonNull
     @Override
     public myAdapter.myViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View mItemView = myInflater.inflate(R.layout.skills, parent, false);
+        return new myViewHolder(mItemView, this);
     }
 
     @Override
     public void onBindViewHolder(@NonNull myAdapter.myViewHolder holder, int position) {
-
+        String mCurrent = skillList.get(position);
+        holder.mySkills.setText(mCurrent);
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return this.skillList.size();
     }
 
     public class myViewHolder extends RecyclerView.ViewHolder{
