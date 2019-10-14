cb9d00ba63c883661b1653d3194de5a7a4dcfa61
FarhanAli325706 <farhanlala706@gmail.com>
2019-10-14 18:29:16 +0500

    beforeAdapter

--- base/src/main/java/com/ahmad/cv/base/MyRecycleView.java
+++ base/src/main/java/com/ahmad/cv/base/MyRecycleView.java
@@ -4,11 +4,23 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MyRecycleView extends AppCompatActivity {
+import java.util.LinkedList;
 
+public class MyRecycleView extends AppCompatActivity {
+    private final LinkedList<String> skillsList=new LinkedList<String>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_recycle_view);
+        skillsList.add("Android");
+        skillsList.add("Java");
+        skillsList.add("Web Development");
+        skillsList.add("PHP");
+        skillsList.add("Java Script");
+        skillsList.add("C++");
+        skillsList.add("C");
+        skillsList.add("SQL");
+        skillsList.add("Assembly");
+        skillsList.add("Python");
     }
 }
